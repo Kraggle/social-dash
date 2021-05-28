f469df0dbcfd0ae64aec65d82a5eb9ac72c479f8
Kraggle <kraggle27@gmail.com>
2021-05-28 01:42:22 +0100

    guzzle test

--- app/Http/Controllers/TestPagesController.php
+++ app/Http/Controllers/TestPagesController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Psr\Http\Message\ServerRequestInterface;
+use GuzzleHttp\Client;
+// use GuzzleHttp\Psr7\Request;
 
 class TestPagesController extends Controller {
     /**
@@ -25,7 +27,23 @@ class TestPagesController extends Controller {
                 'code' => substr($params['code'], 0, -2)
             ]);
 
-            $result = json_decode(file_get_contents("$url?$data"), true);
+            $client = new Client();
+            $response = $client->request('GET', "$url?$data");
+
+            echo $response->getStatusCode(); // 200
+            echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+            echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+
+            // Send an asynchronous request.
+            // $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+            // $promise = $client->sendAsync($request)->then(function ($response) {
+            //     echo 'I completed! ' . $response->getBody();
+            // });
+
+            // $promise->wait();
+
+
+            // $result = json_decode(file_get_contents("$url?$data"), true);
 
             // $ch = curl_init($url);
             // curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode([
@@ -42,23 +60,23 @@ class TestPagesController extends Controller {
             // $result = curl_exec($ch);
             // curl_close($ch);
 
-            error_log(json_encode($result));
+            // error_log(json_encode($result));
 
-            if ($result) {
-                $result['action'] = 'addAccessToken';
+            // if ($result) {
+            //     $result['action'] = 'addAccessToken';
 
-                $ch = curl_init($_ENV['SERVER_URL']);
-                curl_setopt($ch, CURLOPT_HEADER, false);
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($result));
-                curl_exec($ch);
-                curl_close($ch);
+            //     $ch = curl_init($_ENV['SERVER_URL']);
+            //     curl_setopt($ch, CURLOPT_HEADER, false);
+            //     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            //     curl_setopt($ch, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
+            //     curl_setopt($ch, CURLOPT_POST, true);
+            //     curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($result));
+            //     curl_exec($ch);
+            //     curl_close($ch);
 
-                // TODO:: Check response and see if they used the correct account.
-                // TODO:: If the account was different, ask if they want to add it.
-            }
+            //     // TODO:: Check response and see if they used the correct account.
+            //     // TODO:: If the account was different, ask if they want to add it.
+            // }
         }
 
         return view('test.auth', $params);
