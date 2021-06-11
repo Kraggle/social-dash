53d02c30de8342e6cba54eafcc32e5feb2b29658
Kraggle <kraggle27@gmail.com>
2021-06-11 17:14:58 +0100

    Removed useless files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -51,58 +51,6 @@
             @csrf
         </form>
         @include('layouts.page_templates.auth')
-        <div class="fixed-plugin">
-            <div class="dropdown show-dropdown">
-                <a href="#" data-toggle="dropdown">
-                    <i class="fa fa-cog fa-2x"> </i>
-                </a>
-                <ul class="dropdown-menu">
-                    <li class="header-title"> Sidebar Background</li>
-                    <li class="adjustments-line">
-                        <a href="javascript:void(0)" class="switch-trigger background-color">
-                            <div class="badge-colors text-center">
-                                <span class="badge filter badge-primary" data-color="primary"></span>
-                                <span class="badge filter badge-info" data-color="blue"></span>
-                                <span class="badge filter badge-success" data-color="green"></span>
-                                <span class="badge filter badge-warning active" data-color="orange"></span>
-                                <span class="badge filter badge-danger" data-color="red"></span>
-                            </div>
-                            <div class="clearfix"></div>
-                        </a>
-                    </li>
-                    <li class="header-title">
-                        Sidebar Mini
-                    </li>
-                    <li class="adjustments-line">
-                        <div class="togglebutton switch-sidebar-mini">
-                            <span class="label-switch">OFF</span>
-                            <input type="checkbox" name="checkbox" class="bootstrap-switch" data-on-label=""
-                                data-off-label="" />
-                            <span class="label-switch label-right">ON</span>
-                        </div>
-                    </li>
-                    <li class="button-container mt-4">
-                        <a href="https://white-dashboard-pro-laravel.creative-tim.com/docs/getting-started/laravel-setup.html"
-                            target="_blank" class="btn btn-default btn-block btn-round">
-                            Documentation
-                        </a>
-                    </li>
-                    <li class="header-title">Thank you for 95 shares!</li>
-                    <li class="button-container text-center">
-                        <button id="twitter" class="btn btn-round btn-info"><i class="fab fa-twitter"></i> &middot;
-                            45</button>
-                        <button id="facebook" class="btn btn-round btn-info"><i class="fab fa-facebook-f"></i> &middot;
-                            50</button>
-                        <br>
-                        <br>
-                        <a class="github-button"
-                            href="https://github.com/creativetimofficial/ct-white-dashboard-pro-laravel"
-                            data-icon="octicon-star" data-size="large" data-show-count="true" target="_blank"
-                            aria-label="Star ntkme/github-buttons on GitHub">Star</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
     @else
         @include('layouts.page_templates.guest')
     @endif
--- resources/views/pages/example_pages/language.blade.php
+++ resources/views/pages/example_pages/language.blade.php
@@ -719,63 +719,11 @@
           </ul>
           <div class="copyright">
             ©
-            <script>
-              document.write(new Date().getFullYear())
-            </script> made with <i class="tim-icons icon-heart-2"></i> by
+            {{ now()->year }} made with <i class="tim-icons icon-heart-2"></i> by
             <a href="javascript:void(0)" target="_blank">Creative Tim</a> for a better web.
           </div>
         </div>
       </footer>
-    </div>
-  </div>
-  <div class="fixed-plugin">
-    <div class="dropdown show-dropdown">
-      <a href="#" data-toggle="dropdown">
-        <i class="fa fa-cog fa-2x"> </i>
-      </a>
-      <ul class="dropdown-menu">
-        <li class="header-title"> Sidebar Background</li>
-        <li class="adjustments-line">
-          <a href="javascript:void(0)" class="switch-trigger background-color">
-            <div class="badge-colors text-center">
-              <span class="badge filter badge-primary active" data-color="primary"></span>
-              <span class="badge filter badge-info" data-color="blue"></span>
-              <span class="badge filter badge-success" data-color="green"></span>
-              <span class="badge filter badge-warning" data-color="orange"></span>
-              <span class="badge filter badge-danger" data-color="red"></span>
-            </div>
-            <div class="clearfix"></div>
-          </a>
-        </li>
-        <li class="header-title">
-          Sidebar Mini
-        </li>
-        <li class="adjustments-line">
-          <div class="togglebutton switch-sidebar-mini">
-            <span class="label-switch">OFF</span>
-            <input type="checkbox" name="checkbox" checked class="bootstrap-switch" data-on-label="" data-off-label="" />
-            <span class="label-switch label-right">ON</span>
-          </div>
-          <div class="togglebutton switch-change-color mt-3">
-            <span class="label-switch">LIGHT MODE</span>
-            <input type="checkbox" name="checkbox" checked class="bootstrap-switch" data-on-label="" data-off-label="" />
-            <span class="label-switch label-right">DARK MODE</span>
-          </div>
-        </li>
-        <li class="button-container mt-4">
-          <a href="../../docs/getting-started/laravel-setup.html" class="btn btn-default btn-block btn-round">
-            Documentation
-          </a>
-        </li>
-        <li class="header-title">Thank you for 95 shares!</li>
-        <li class="button-container text-center">
-          <button id="twitter" class="btn btn-round btn-info"><i class="fab fa-twitter"></i> &middot; 45</button>
-          <button id="facebook" class="btn btn-round btn-info"><i class="fab fa-facebook-f"></i> &middot; 50</button>
-          <br>
-          <br>
-          <a class="github-button" href="https://github.com/creativetimofficial/ct-white-dashboard-pro" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star ntkme/github-buttons on GitHub">Star</a>
-        </li>
-      </ul>
     </div>
   </div>
   <!--   Core JS Files   -->
@@ -835,43 +783,6 @@
         white_color = false;
 
         window_width = $(window).width();
-
-        fixed_plugin_open = $('.sidebar .sidebar-wrapper .nav li.active a p').html();
-
-
-
-        $('.fixed-plugin a').click(function(event) {
-          if ($(this).hasClass('switch-trigger')) {
-            if (event.stopPropagation) {
-              event.stopPropagation();
-            } else if (window.event) {
-              window.event.cancelBubble = true;
-            }
-          }
-        });
-
-        $('.fixed-plugin .background-color span').click(function() {
-          $(this).siblings().removeClass('active');
-          $(this).addClass('active');
-
-          var new_color = $(this).data('color');
-
-          if ($sidebar.length != 0) {
-            $sidebar.attr('data', new_color);
-          }
-
-          if ($main_panel.length != 0) {
-            $main_panel.attr('data', new_color);
-          }
-
-          if ($full_page.length != 0) {
-            $full_page.attr('filter-color', new_color);
-          }
-
-          if ($sidebar_responsive.length != 0) {
-            $sidebar_responsive.attr('data', new_color);
-          }
-        });
 
         $('.switch-sidebar-mini input').on("switchChange.bootstrapSwitch", function() {
           var $btn = $(this);
